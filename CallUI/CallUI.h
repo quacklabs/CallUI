72afdba776f9d612a5f33e8f1385756c0e3e322e
Mark Boleigha <donhoenix@gmail.com>
2022-04-12 21:10:22 +0100

    Initial Commit

---
+++ CallUI/CallUI.h
@@ -0,0 +1,19 @@
+//
+//  CallUI.h
+//  CallUI
+//
+//  Created by Mark Boleigha on 12/04/2022.
+//  Copyright © 2022 Code HK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CallUI.
+FOUNDATION_EXPORT double CallUIVersionNumber;
+
+//! Project version string for CallUI.
+FOUNDATION_EXPORT const unsigned char CallUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CallUI/PublicHeader.h>
+
+
